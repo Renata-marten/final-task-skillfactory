f7648751eb1da37ae0990d0016cac72bca78c44c
Renata-marten <ren_ish@mail.ru>
2021-01-17 15:15:11 +0500

    added some changes and files config and required.txt

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -8,23 +8,23 @@ from pages.main_page import MainPage
 
 ''' для каждой страницы пишем тесты, используя элементы предварительно описанные в файле страницы '''
 
-# def test_click_gif(web_browser):
-#     """ Проверим переход по лого-гифке в хедере. """
-#     page = MainPage(web_browser)
-#     page.logo_gif.click()
-#     # проверим, произошел ли переход с главной страницы
-#     assert page.get_domain() == config.ali_domain, "error clicking gif in header"
-#
-#     time.sleep(5)
-#     web_browser.back()
-#     # возвращаемся на предыдущую страницу
-#
-# def test_click_logo_ali(web_browser):
-#     """ Проверим переход по лого алиэкспресса в верхней панели страницы """
-#     page = MainPage(web_browser)
-#     page.logo_ali.click()
-#     # проверим, произошел ли переход с главной страницы
-#     assert page.get_domain() == config.ali_domain, "error clicking gif in header"
+def test_click_gif(web_browser):
+    """ Проверим переход по лого-гифке в хедере. """
+    page = MainPage(web_browser)
+    page.logo_gif.click()
+    # проверим, произошел ли переход с главной страницы
+    assert page.get_domain() == config.ali_domain, "error clicking gif in header"
+
+    time.sleep(5)
+    web_browser.back()
+    # возвращаемся на предыдущую страницу
+
+def test_click_logo_ali(web_browser):
+    """ Проверим переход по лого алиэкспресса в верхней панели страницы """
+    page = MainPage(web_browser)
+    page.logo_ali.click()
+    # проверим, произошел ли переход с главной страницы
+    assert page.get_domain() == config.ali_domain, "error clicking gif in header"
 
 def test_check_main_search(web_browser):
     """ Проверим работу поиска в шапке """
